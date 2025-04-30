19eab69d714af06664a8fc61e7484c3aad9b2fd8
KiramZodiac <kiramzodiac@gmail.com>
2025-04-30 14:23:20 +0300

    added framer motion

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -50,7 +50,7 @@ const projects: Project[] = [
 
 export default function ProjectsPage() {
   return (
-    <section className="min-h-screen p-6 md:p-12 bg-gray-50">
+    <section className="min-h-screen p-6 md:p-12 bg-gray-50 max-lg:pt-20">
       <h1 className="text-4xl font-bold text-center mb-12 text-gray-800">My Projects</h1>
       <div className="grid gap-10 md:grid-cols-2 lg:grid-cols-3">
         {projects.map((project, index) => (
